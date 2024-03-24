4077f16a56ee7a5310abcf5a066ab47f650a4f03
CaptainHarlockSSX <thedarklink12mc@gmail.com>
2024-03-24 12:03:47 +0100

    Implement a first draft of the CounterCardView with static sample data.

---
+++ Who Is The Best/Views/CounterCardView.swift
@@ -0,0 +1,36 @@
+//
+//  CounterCardView.swift
+//  Who Is The Best
+//
+//  Created by Nathan Rassié on 24/03/2024.
+//
+
+import SwiftUI
+
+struct CounterCardView: View {
+    var body: some View {
+		HStack {
+			Image(systemName: "camera")
+				.resizable(resizingMode: .tile)
+				.padding(.horizontal)
+			
+			VStack {
+				Text("Counter Name")
+					.font(.headline)
+					.accessibilityAddTraits(.isHeader)
+				Text("Leader : Antoine")
+					.font(.subheadline)
+					.accessibilityAddTraits(.isStaticText)
+				Spacer()
+				Text("This is a description of this Counter.")
+					.font(.caption)
+					.italic()
+			}
+			.padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+		}
+    }
+}
+
+#Preview {
+    CounterCardView()
+}
