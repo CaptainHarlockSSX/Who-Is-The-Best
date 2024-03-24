38815f5bc3001d157eab600cfa18ec634f0b5b74
CaptainHarlockSSX <thedarklink12mc@gmail.com>
2024-03-24 10:51:44 +0100

    Remove default code and prepare CountersView to be developed

---
+++ Who Is The Best/Views/CountersView.swift
@@ -0,0 +1,18 @@
+//
+//  CountersView.swift
+//  Who Is The Best
+//
+//  Created by Nathan Rassié on 24/03/2024.
+//
+
+import SwiftUI
+
+struct CountersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CountersView()
+}
