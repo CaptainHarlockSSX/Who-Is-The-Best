45832eb4a343b00a995f2c0c1a1c2b331315fedb
CaptainHarlockSSX <thedarklink12mc@gmail.com>
2024-04-24 17:18:51 +0200

    Complete missing attributes and functions in Counter model

--- Who Is The Best/Models/Counter.swift
+++ Who Is The Best/Models/Counter.swift
@@ -7,23 +7,43 @@
 
 import Foundation
 import SwiftData
+import SwiftUI
 
 @Model
 final class Counter: Identifiable {
 	let id: UUID
 	var name: String
+	var caption: String?
 	var score: Dictionary<String, Int>
 	@Relationship(deleteRule: .noAction, inverse: \User.counters) var counterParticipants = [User]()
 	var type: CounterType
 	var history: [CounterHistory]
+	var image: Image?
 	
-	init(id: UUID = UUID(), name: String, type: CounterType, score: Dictionary<String, Int> = [:], history: [CounterHistory] = [], counterParticipants: [User] = []) {
+	init(id: UUID = UUID(), name: String, caption: String? = nil, score: Dictionary<String, Int> = [:], counterParticipants: [User] = [User](), type: CounterType, history: [CounterHistory] = [], image: Image? = nil) {
 		self.id = id
 		self.name = name
+		self.caption = caption
 		self.score = score
+		self.counterParticipants = counterParticipants
 		self.type = type
 		self.history = history
-		self.counterParticipants = counterParticipants
+		self.image = image
+	}
+	
+	func addParticipant(participant: User) -> Void {
+		self.score[participant.username] = 0
+		counterParticipants.append(participant)
+	}
+	
+	func removeParticipant(withName name: String) -> Void {
+		self.score.removeValue(forKey: name)
+		self.counterParticipants.removeAll(where: { $0.username == name })
+	}
+	
+	func removeParticpant(withIndex index: Int) -> Void {
+		let removedUser = self.counterParticipants.remove(at: index)
+		self.score.removeValue(forKey: removedUser.username)
 	}
 }
 
